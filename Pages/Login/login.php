689d2b50ec6c71bb58fc7f8d36693edd927764ee
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2021-10-25 22:33:31 -0400

    Create login.php

---
+++ Pages/Login/login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Undergraduate Research Database</title>
+    <meta charset="utf-8".>
+    <meta name="viewport" content="width=device-width, initial scale=1">
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <title>Signin Template for Bootstrap</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+        <style>
+        img {
+          border-radius: 17%;
+        }
+        </style>
+  </head>
+  <body>
+    <div class="container">
+    <div class="row" style="width: auto;height: 150px;">
+            <div class="col-4"></div>
+              <div class="col-3">
+        <div class="text-center">
+        <form class="form-signin">
+        <div class="imageRound">
+                <img class="mb-4" src="https://s3-us-west-2.amazonaws.com/webresources-savingforcollege/images/school_logos/salisbury-university.png" alt="" width="128" height="128">
+        </div>
+        <h1 class="h3 mb-3 font-weight-normal">Sign Into Undergraduate Research Database</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2021-2021</p>
+    </form>
+      </div>
+      </div>
+      </div>
+    </div>
+    </div>
+  </body>
+</html>

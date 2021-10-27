a824f46bfdaaf4212f8cc115a06a873cdd264099
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2021-10-27 10:10:17 -0400

    Allows user to select table and attribute

--- Pages/edit/edit.php
+++ Pages/edit/edit.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <body>
 <?php
@@ -7,17 +8,23 @@
  else{
          echo "Connection Error";
  }
-echo"\nDEPARTMENT ONLY. THIS IS FOR TESTING";
+ echo"\nDEPARTMENT ONLY. THIS IS FOR TESTING";
  echo"<form name=\"getUsr\" action =\"\" method\"get\">
-         <input type=\"text\" name=\"search\" id=\"search\" placeholder=\"What you would like to change\" style=\"width:300px;\"><br>
+         <input type=\"text\" name=\"table\" id=\"table\" placeholder=\"Enter table name\" style=\"width:300px;\"><br>
+        <input type=\"text\" name=\"attribute\" id=\"attribute\" placeholder=\"Which attribute would you like to change\" style=\"width:300px;\"><br>
+         <input type=\"text\" name=\"search\" id=\"search\" placeholder=\"What value in the attribute would you like to change\" style=\"width:300px;\"><br>
          <input type=\"text\" name=\"changeTo\" id=\"changeTo\" placeholder=\"What you would like to change it to\" style=\"width:300px;\"><br><br>
-        <input type=\"submit\" value=\"Submit\">
+        <input type=\"submit\" value=\"Submit\"> 
         </form>";
  $search=$_GET['search'];
  echo "Search =$search\n";
  $replaceWith=$_GET['changeTo'];
  echo "Replace with=$replaceWith\n";
-$query = "UPDATE Department SET Name=\"$replaceWith\" WHERE Name=\"$search\"";
+ $attribute=$_GET['attribute'];
+ echo "Attribute=$attribute";
+ $table=$_GET['table'];
+ echo "Table=$table";
+$query = "UPDATE $table SET $attribute=\"$replaceWith\" WHERE $attribute=\"$search\"";
 $d = mysqli_query($connect, $query);
 if(!$d){
         echo "The query failed";

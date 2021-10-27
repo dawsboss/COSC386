b62a20a75679d783e66aea07b00caf08e347b5a9
Grant Dawson <gdawson2018@gmail.com>
2021-10-27 09:51:29 -0400

    Finish Login, Admin, and Account creating

--- Pages/BackEnd.php
+++ Pages/BackEnd.php
@@ -21,15 +21,30 @@ if((isset($_GET["q"]) || isset($_GET["f"])) && $_GET["q"] && $_GET["f"]){
   while($data = mysqli_fetch_array($Ssql)){
     array_push($search, $data);
   }
+  print_r($search);
 }
 
 #Login Page Quering and Access
 #To use: Post with username and passhash - username and the password hashed
 #Returns: logged in status, admin status
-# logged - *username*
-# admin - T/F 
+# session->logged - *username*
+# session->admin - T/F 
 if(isset($_POST['username']) && $_POST['username'] && isset($_POST['passhash']) && $_POST['passhash']){
-#LOGIN STUFF
+  #LOGIN STUFF
+  $logUser = null;
+  $Luname = mysqli_real_escape_string($connect,$_POST["username"]);
+  $Lpass = mysqli_real_escape_string($connect,$_POST["passhash"]);
+  $Lquery = "SELECT * FROM Login WHERE Username = \"$Luname\"";
+  $Lsql = mysqli_query($connect, $Lquery);
+  $logUser = mysqli_fetch_array($Lsql);
+  if(logUser){#There is a username found
+    if(passhash == logUser['Password']){#Then valid login
+      $_SESSION['logged'] = $Luname;
+    }
+    if(logUser['Admin'] == 1){#User was an admin TODO changed based on Login table values
+      $_SESSION['admin'] = true;
+    }
+  }
 
 }
 
@@ -43,7 +58,7 @@ if(isset($_GET['p']) && $_GET['p']){
   $Pusername = mysqli_real_escape_string($connect,$_GET['p']);
   $Pquery = "SELECT * FROM Professor WHERE Email LIKE \"$Pusername%\"";#TODO turn like to = when fix email att stuff
   $Psql = mysqli_query($connect,$Pquery);
-  $profile = mysqli_fetch($Psql);
+  $profile = mysqli_fetch_array($Psql);
 }
 
 #Research Page Quering
@@ -56,33 +71,56 @@ if(isset($_GET['r']) && $_GET['r']){
   $RID = mysqli_real_escape_string($connect, $_GET['r']);
   $Rquery = "SELECT * FROM Research WHERE ID = $RID";#TODO idk if this will work yet... string stuff no fun
   $Rsql = mysqli_query($connect,$Rquery);
-  $research = mysqli_fetch($);
+  $research = mysqli_fetch_array($Rsql);
 }
 
 #Editing Page Querying
 #To use: Post using 'eu' editUsername
 #Returns: A querry of current infomation in DB
+# Eprofile - Tuple of all profile attributes
 if(isset($_POST['eu']) && $_POST['eu']){
   #EDIT STUFF
-
+  $Eprofile = null;
+  $Eusername = mysqli_real_escape_string($connect,$_POST['eu']);
+  $Equery = "SELECT * FROM Professor WHERE Username LIKE \"$Eusername%\"";#TODO turn like to = when fix email att stuff
+  $Esql = mysqli_query($connect,$Equery);
+  $Eprofile = mysqli_fetch_array($Esql);
 }
 #Editing Page Alter
 #To use: Post using 'ea' editAalter
 #Returns: Nothing
 #Will update a tuple matching the login
-if(isset($_POST['ea']) && $_POST['ea']){
+if(isset($_POST['ename']) && $_POST['ename'] && isset($_POST['euname']) && $_POST['euname'] && isset($_POST['edname']) && $_POST['edname'] && isset($_POST['esname']) && $_POST['esname'] && isset($_POST['erstate']) && $_POST['erstate'] && isset($_POST['eweb']) && $_POST['eweb'] && isset($_POST['eaval']) && $_POST['eaval'] && isset($_POST['ebio']) && $_POST['ebio'] && isset($_POST['ephone']) && $_POST['ephone']){
   #EDIT ALTER
-
+  
 }
+
 
 
 #Creating Page Insert
 #To use: Post using 'ci' createInsert
-#Returns: Nothing
+#Returns: SESSION -> create: T/F
 #Will create new user can only be down by admin
-if(isset($_POST['ci']) && $_POST['ci']){
-#CREATE STUFF
+if(isset($_POST['admin']) &&  $_POST['admin']== true){
+  if(isset($_POST['cname']) && $_POST['cname'] && isset($_POST['cuname']) && $_POST['cuname'] && isset($_POST['cdname']) && $_POST['cdname'] && isset($_POST['csname']) && $_POST['csname'] && isset($_POST['crstate']) && $_POST['crstate'] && isset($_POST['cweb']) && $_POST['cweb'] && isset($_POST['caval']) && $_POST['caval'] && isset($_POST['cbio']) && $_POST['cbio'] && isset($_POST['cphone']) && $_POST['cphone']){
+  #CREATE STUFF
+    $Cname = mysqli_real_escape_string($connect,$_POST['cname']);
+    $Cuname = mysqli_real_escape_string($connect,$_POST['cuname']);
+    $Cdeptname = mysqli_real_escape_string($connect,$_POST['cdname']);
+    $Cschoolname = mysqli_real_escape_string($connect,$_POST['csname']);
+    $CresearchState = mysqli_real_escape_string($connect,$_POST['crstate']);
+    $Cweb = mysqli_real_escape_string($connect,$_POST['cweb']);
+    $Caval = mysqli_real_escape_string($connect,$_POST['caval']);
+    $Cbio = mysqli_real_escape_string($connect,$_POST['cbio']);
+    $Cphone = mysqli_real_escape_string($connect,$_POST['cphone']);
 
+
+    $Cquery = "INSERT INTO Professor VALUES($Cname, $Cuname, $Cdeptname, $Cschoolname, $CresearchState, $Cweb, $Caval, $Cbio, $Cphone)";#TODO turn like to = when fix email att stuff
+    $Csql = mysqli_query($connect,$Cquery);
+    if($Csql){
+      $_SESSION['create'] = true;
+    }
+  }
 }
 
 mysqli_close($connect);

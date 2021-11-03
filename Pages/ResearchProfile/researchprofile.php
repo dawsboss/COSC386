6363f464750649774dbff8048a42b8a40ad24458
Grant Dawson <gdawson2018@gmail.com>
2021-11-03 15:55:27 -0400

    Creating header nav bar file and implemet it

--- Pages/ResearchProfile/researchprofile.php
+++ Pages/ResearchProfile/researchprofile.php
@@ -23,35 +23,7 @@ endif;
  </head>
 <body>
     <header class="header" style="position: relative; top: -50px;">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Salisbury University</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Search</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-      <!--<li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-       s <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>-->
-    </ul>
-  </div>
-</nav>
+    <?php include ("../navbar.php");?>
         <div class="jumbotron jumbotron-fluid", style="width: auto; height: auto;">
           <div class="container">
           <?php
--- Pages/Search/testing.php
+++ Pages/Search/testing.php
@@ -13,38 +13,10 @@
 <body>
     <?php
         include ("../BackEnd.php");
+        include ("../navbar.php");
         session_start();
     ?>
     <header class="header">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Salisbury University</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Search<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-      <!--<li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>-->
-    </ul>
-  </div>
-</nav>
         <div class="jumbotron jumbotron-fluid" , style="width: auto; height: 170px;">
             <div class="container">
                 <div class="row">
@@ -58,7 +30,7 @@
     <div class="container">
     <form method="get">
         <div class="row">
-            <div class="col-11">
+            <div class="col-10">
                 <div class="input-group">
                     <input type="text" class="form-control" aria-label="Text input with dropdown button" name="q">
                     <div class="input-group-append">
@@ -73,6 +45,7 @@
                     </div>
                 </div>
             </div>
+            <div class="col-1"></div>
             <div class="col-1">
                 <input class="btn btn-outline-secondary float-right" type="submit">
             </div>

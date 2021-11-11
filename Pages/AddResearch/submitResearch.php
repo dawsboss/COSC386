a05dc036e9b5f5e52f02a55854be52b17eab8824
jventura1738 <justinv9001@aol.com>
2021-11-11 18:27:16 -0500

    Need to fix

--- Pages/AddResearch/submitResearch.php
+++ Pages/AddResearch/submitResearch.php
@@ -4,6 +4,11 @@
 
 if (isset($_POST['Submit'])) {
     $research_title = $_POST['rtitle'];
+    $research_desc = $_POST['rdesc'];
+    $research_abstract = $_POST['rabstract'];
+
+    $query = "INSERT INTO Research
+              VALUES($research_desc, $research_abstract, 'NA', $research_title, 0)";
 }
 
 ?>

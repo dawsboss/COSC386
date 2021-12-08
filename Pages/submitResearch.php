169f53919bf408a20e6a38d81d2eaeb3226eb33e
jventura1738 <justinv9001@aol.com>
2021-12-08 01:36:06 -0500

    Add backend for new fields

--- Pages/submitResearch.php
+++ Pages/submitResearch.php
@@ -58,38 +58,112 @@
         $h_query->execute();
         $h_query->close();
         $r_query->close();
+
+        # If there is a grant, add it:
+        if ($isset($_POST['gamount'])) {
+            $g_query = $connect->prepare("INSERT INTO Grants(Amount, year, Organization) VALUES(?, ?, ?)");
+            $g_query->bind_param(
+                "iis",
+                $_POST['gamount'],
+                $_POST['gyear'],
+                $_POST['gorg']
+            );
+            $g_query->execute();
+            $g_query->close();
+            $g_id = $connect->insert_id;
+
+            $fundedby_query = $connect->prepare("INSERT INTO FundedBy(GrantID, researchID) VALUES(?, ?)");
+            $fundedby_query->bind_param(
+                "ii",
+                $g_id,
+                $r_id
+            );
+            $fundedby_query->execute();
+            $fundedby_query->close();
+        }
+        # If there is a student, add them:
+        if (isset($_POST['sname'])) {
+            $s_query = $connect->prepare("INSERT INTO Student(Email, Name) VALUES(?, ?)");
+            $s_query->bind_param(
+                "ss",
+                $_POST['semail'],
+                $_POST['sname']
+            );
+            $s_query->execute();
+            $s_query->close();
+            $s_id = $connect->insert_id;
+
+            $workon_query = $connect->prepare("INSERT INTO WorkOn(researchID, studentEmail) VALUES(?, ?)");
+            $workon_query->bind_param(
+                "is",
+                $r_id,
+                $_POST['semail']
+            );
+            $workon_query->execute();
+            $workon_query->close();
+        }
     }
     $connect->close();
 
     ?>
 
     <div class="container mt-5">
-        <form name="submit" action="https://lamp.salisbury.edu/~jventura3/COSC386/Pages/AddResearch/submitResearch.php" method="post">
+        <form name="submit" action="https://lamp.salisbury.edu/~jventura3/COSC386/Pages/submitResearch.php" method="post">
             <div class="row g-3 mb-2">
                 <div class="col-md-12">
                     <label for="researchTitle">Research Title</label>
                     <div class="row g-3">
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="rtitle">
+                            <input type="text" class="form-control shadow-sm rounded" name="rtitle">
                         </div>
                         <div class="col-md-2">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <input class="form-check-input shadow-sm rounded" type="checkbox" value="" name="ongoing" id="flexCheckDefault">
                             <label class="form-check-label" for="flexCheckDefault">Ongoing Research?</label>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row g-3">
+            <div class="row g-3 mb-2">
+                <div class="col-md-12">
+                    <label for="researchLink">Link</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="rlink">
+                </div>
+            </div>
+            <div class="row g-3 mb-2">
                 <div class="col-md-12 mb-2">
                     <label for="researchDesc">Research Description</label>
-                    <textarea class="form-control" rows="5" name="rdesc"></textarea>
+                    <textarea class="form-control shadow-sm rounded" rows="5" name="rdesc"></textarea>
                 </div>
                 <div class="col-md-12 mb-2">
                     <label for="researchAbstract">Abstract</label>
-                    <textarea class="form-control" rows="5" name="rabstract"></textarea>
+                    <textarea class="form-control shadow-sm rounded" rows="5" name="rabstract"></textarea>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+            <div class="row g-3 mb-2">
+                <div class="col-md-2 mb-2">
+                    <label for="grantAmount">Grant Amount (USD)</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="gamount">
+                </div>
+                <div class="col-md-2 mb-2">
+                    <label for="grantYear">Grant Year</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="ryear">
+                </div>
+                <div class="col-md-8 mb-2">
+                    <label for="grantOrg">Organization</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="gorg">
+                </div>
+            </div>
+            <div class="row g-3 mb-2">
+                <div class="col-md-6">
+                    <label for="studentName">Student Researcher Name</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="sname">
+                </div>
+                <div class="col-md-6">
+                    <label for="studentEmail">Student Email</label>
+                    <input type="text" class="form-control shadow-sm rounded" name="semail">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary shoadow-sm" name="submit">Submit</button>
         </form>
     </div>
 </body>
